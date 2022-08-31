cbdeff22e79907490e68746c8bebfb34317b172c
Daniel <daniel@budsoft.co.il>
2022-08-31 17:17:07 +0300

    callback -> jkpool validation

--- contracts/vRouter.sol
+++ contracts/vRouter.sol
@@ -68,18 +68,24 @@ contract vRouter is IvRouter, Multicall {
             (SwapCallbackData)
         );
 
-        if (decodedData.jkPool > address(0))
+        if (decodedData.jkPool > address(0)) {
+            //validate JK pool
+            (address jkToken0, address jkToken1) = IvPair(decodedData.jkPool)
+                .getTokens();
+
             require(
-                msg.sender == decodedData.jkPool,
+                msg.sender ==
+                    PoolAddress.computeAddress(factory, jkToken0, jkToken1),
                 "VSWAP:INVALID_CALLBACK_VPOOL"
             );
-        else
+        } else {
             require(
                 msg.sender ==
                     PoolAddress.computeAddress(factory, tokenIn, tokenOut),
                 "VSWAP:INVALID_CALLBACK_POOL"
             );
-
+        }
+        
         //validate amount to pay back dont exceeds
         require(
             requiredBackAmount <= decodedData.tokenInMax,
