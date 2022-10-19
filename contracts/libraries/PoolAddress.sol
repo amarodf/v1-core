873a1b13c4247627e6cf961ce404db3abd9735ee
Daniel <daniel@budsoft.co.il>
2022-10-20 00:41:58 +0300

    Pool address and migrations

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -5,9 +5,9 @@ pragma solidity 0.8.2;
 /// @title Provides functions for deriving a pool address from the factory and token
 library PoolAddress {
     //local 0x5e2c53146a5dae93216e921083c77cff22430a938b441d1d1e4754f93a3d1f3f
-    //mumbai 0x5db6861683206e826938bc737d61cb87fad9c12f0c8de68fe85038b36a6a7d5f
+    //mumbai 0x501ab8b57f651e35036ffa4e1e289a76d14924a4b6201272ac3800e7959700cd
     bytes32 internal constant POOL_INIT_CODE_HASH =
-        0x5e2c53146a5dae93216e921083c77cff22430a938b441d1d1e4754f93a3d1f3f;
+        0x501ab8b57f651e35036ffa4e1e289a76d14924a4b6201272ac3800e7959700cd;
 
 
     function orderAddresses(address tokenA, address tokenB)
